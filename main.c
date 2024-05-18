b98ac60e3b757eef0302db2ff4a0544d34422729
supremex04 <bhandarisupreme7@gmail.com>
2024-05-18 19:14:35 +0545

    image read

--- main.c
+++ main.c
@@ -7,6 +7,12 @@
 #include "stb_image/stb_image_write.h"
 
 int main() {
-    printf("Hello");
+    int width, height, channels;
+    unsigned char *img = stbi_load("sunflower.jpg", &width, &height, &channels,0);
+    if (img == NULL){
+        printf("Error in loading the image \n");
+        exit(1);
+    }
+    printf("[IMAGE LOAD SUCCESS] \nWidth: %dpx, Height: %dpx and channels: %d \n", width, height, channels);
     return 0;
 }
