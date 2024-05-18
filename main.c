0098f0cfd00070ae7d5b3b29eeb14c57f1a8b2fc
supremex04 <bhandarisupreme7@gmail.com>
2024-05-18 20:49:08 +0545

    greyscale

--- main.c
+++ main.c
@@ -14,5 +14,27 @@ int main() {
         exit(1);
     }
     printf("[IMAGE LOAD SUCCESS] \nWidth: %dpx, Height: %dpx and channels: %d \n", width, height, channels);
+    printf("works2");
+    
+
+    size_t img_size = width * height *channels;
+    int grey_channels = channels == 4 ? 2 : 1;
+    size_t grey_img_size = width * height * grey_channels;
+    printf("works1");
+    unsigned char *grey_img = malloc(grey_img_size);
+    printf("works");
+    if (grey_img == NULL){
+        printf("Error allocating space to greyscale image!");
+    }
+
+    for (unsigned char *p = img, *pg =grey_img; p != img +img_size; p += channels, pg += grey_channels ){
+        *pg = (uint8_t)((*p + *(p+1) +*(p+2))/3.0);
+        if (channels == 4){
+            *(pg+1) = *(p+3);
+        }
+    }
+    stbi_write_jpg("sunflower_grey.png", width, height, grey_channels, grey_img, 100);
+    stbi_image_free(img);
+    free(grey_img);
     return 0;
 }
