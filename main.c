a17ea20a2bad836ca458166c25587380dd715106
supremex04 <bhandarisupreme7@gmail.com>
2024-05-21 15:15:45 +0545

    manip

--- main.c
+++ main.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
+int width, height, channels;
+unsigned char *img;
+unsigned char *manip_img;
 
-int main() {
-    int width, height, channels;
-    unsigned char *img = stbi_load("sunflower.jpg", &width, &height, &channels,0);
+void printCharInBinary(unsigned char c) {
+    for (int i = 7; i >= 0; i--) {
+        // bitwise and with each bit
+        printf("%d",(c & (1 << i)) ? 1 : 0);
+    }
+}
+
+void read_image(){
+    img = stbi_load("sunflower.jpg", &width, &height, &channels,0);
     if (img == NULL){
         printf("Error in loading the image \n");
         exit(1);
     }
     printf("[IMAGE LOAD SUCCESS] \nWidth: %dpx, Height: %dpx and channels: %d \n", width, height, channels);
-    
+}
 
+int main(){
+    read_image();
     size_t img_size = width * height *channels;
-    int grey_channels = channels == 4 ? 2 : 1;
-    size_t grey_img_size = width * height * grey_channels;
-    unsigned char *grey_img = malloc(grey_img_size);
-    if (grey_img == NULL){
-        printf("Error allocating space to greyscale image!");
+    manip_img = malloc(img_size);
+    char input[100];
+    printf("Enter a string: ");
+    // Read input until a newline is encountered or buffer is full
+    scanf("%255[^\n]", input);
+    printf("Binary representation of the string:\n");
+    // size_t is unsigned integer type
+    for (size_t i = 0; i < strlen(input); i++) {
+        printCharInBinary(input[i]);
+        printf(" "); 
     }
-    unsigned int combined_color = (img[0] << 16) | (img[1] << 8) | img[2];
-    printf("First pixel RGB: #%06X\n", combined_color);
-    
-    for (unsigned char *p = img, *pg =grey_img; p != img +img_size; p += channels, pg += grey_channels ){
+    printf("\n");
+    for (unsigned char *p = img, *pg =manip_img; p != img + img_size; p += 1, pg +=1){
 
-        *pg = (uint8_t)((*p + *(p+1) +*(p+2))/3.0);
-        if (channels == 4){
-            *(pg+1) = *(p+3);
-        }
+        *pg = (uint8_t)(*p | (0x7F) );
+        
     }
-    stbi_write_jpg("sunflower_grey.png", width, height, grey_channels, grey_img, 100);
+    stbi_write_jpg("sunflower_manip.png", width, height, channels, manip_img, 100);
     stbi_image_free(img);
-    free(grey_img);
+    free(manip_img);
+    
+    
     return 0;
 }
--- test.c
+++ test.c
@@ -9,6 +9,7 @@
 
 int width, height, channels;
 unsigned char *img;
+unsigned char *manip_img;
 
 void printCharInBinary(unsigned char c) {
     for (int i = 7; i >= 0; i--) {
@@ -23,29 +24,36 @@ void read_image(){
         printf("Error in loading the image \n");
         exit(1);
     }
-    printf("[IMAGE LOAD SUCCESS] \nWidth: %dpx, Height: %dpx and channels: %d \n", width, height, channels);
+    printf("[IMAGE LOAD SUCCESS] \nWidth: %dpx, Height: %dpx and Channels: %d \n", width, height, channels);
 }
 
 int main(){
-    int width, height, channels;
-    unsigned char *img = stbi_load("sunflower.jpg", &width, &height, &channels,0);
-    if (img == NULL){
-        printf("Error in loading the image \n");
-        exit(1);
-    }
-    printf("[IMAGE LOAD SUCCESS] \nWidth: %dpx, Height: %dpx and channels: %d \n", width, height, channels);
+    read_image();
+    size_t img_size = width * height *channels;
+    manip_img = malloc(img_size);
     char input[100];
-    printf("Enter a string: ");
-    // Read input until a newline is encountered or buffer is full
-    scanf("%255[^\n]", input);
-    printf("Binary representation of the string:\n");
-    // size_t is unsigned integer type
-    for (size_t i = 0; i < strlen(input); i++) {
-        printCharInBinary(input[i]);
-        printf(" "); 
-    }
-
+    // printf("Enter a string: ");
+    // // Read input until a newline is encountered or buffer is full
+    // scanf("%255[^\n]", input);
+    // printf("Binary representation of the string:\n");
+    // // size_t is unsigned integer type
+    // for (size_t i = 0; i < strlen(input); i++) {
+    //     printCharInBinary(input[i]);
+    //     printf(" "); 
+    // }
     printf("\n");
+    for (unsigned char *p = img, *pg =manip_img; p != img + img_size; p += 1, pg +=1){
+
+        *pg = (*p & 0xF0 );
+        
+    }
+    for (int i = 0; i < 10; i++) {
+    printf("Original: %d, Modified: %d\n",img[i],  manip_img[i]);
+    }
+    stbi_write_jpg("sunflower_manip.png", width, height, channels, manip_img, 100);
+    stbi_image_free(img);
+    free(manip_img);
+    
     
     return 0;
 }
